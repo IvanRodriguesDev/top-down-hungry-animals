0da7281557f266f20cd7d0ce349d67362cbf267f
Ivan Rodrigues <104801431+IvanRodriguesDev@users.noreply.github.com>
2023-08-02 18:59:41 -0300

    Atualização No Codigo

--- top-down-hungry-animals/Assets/Course Library/Scripts/PlayerController.cs
+++ top-down-hungry-animals/Assets/Course Library/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 30.0f;
+    public float xRange = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,9 +18,14 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
 
-        if (transform.position.x < -10)
+        if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
 
