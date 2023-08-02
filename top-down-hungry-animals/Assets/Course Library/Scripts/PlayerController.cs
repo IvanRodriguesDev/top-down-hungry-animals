33e5aee9424018d4cf98d397174f6c2c0e82939d
Ivan Rodrigues <104801431+IvanRodriguesDev@users.noreply.github.com>
2023-08-02 19:51:02 -0300

    Atualização no Codigo

--- top-down-hungry-animals/Assets/Course Library/Scripts/PlayerController.cs
+++ top-down-hungry-animals/Assets/Course Library/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 30.0f;
     public float xRange = 10.0f;
+    public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -32,6 +33,12 @@ public class PlayerController : MonoBehaviour
 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           // Launch a projectile from the player
+           Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
         
     }
 }
